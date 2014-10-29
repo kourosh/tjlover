d769666eb8cc06785932b3051986a69063bb9605
Kourosh Karimkhany <kouroshk@gmail.com>
2014-10-29 11:40:09 -0700

    Add logic to app.js to grab a random product out of the database

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require("express"),
 app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // enable ejs
 app.set("view engine", "ejs");
@@ -25,19 +25,56 @@ app.use(express.static(__dirname + '/public'));
 
 // Route for home page
 app.get("/", function(req, res) {
-	//Step 1: Pull out all ids into an array
-	models.Product.find(8).success(function(item) {
-		res.render("index.ejs", {
-			product: item.name,
-			description: item.description,
-			picurl: item.picurl
-		});
-	});
-});
-	//Step 2: Generate random number from 0 to array.length - 1
-	//Step 3: Pull record with that ID from db
-	//Step 4: Repeat
+	// Need to pick three items at random from the Product table in the 
+	// database to display on the front page. To do this, we have to first
+	// find all the available products, because products could be added or 
+	// deleted in real time.
 
+	// Start with a Sequelize query to find all the items in the 
+	// Product table.
+	models.Product.findAll().done(function(error, items) {
+		
+		// Create blank array for storing IDs of all returned products
+		var productIds= [];
+
+		// Push the primary key IDs of the returned items into the array
+		for (var i=0; i < items.length; i++) {
+			productIds.push(items[i].id);
+		};
+
+		console.log(productIds);
+
+		// This function generates a random number from 0 to the number
+		// of items in the Products table
+		randomItem = function() {
+			return Math.floor(Math.random()*productIds.length - 1);
+		};
+
+		// Assign the random number to variable randomPickedItem. This
+		// variable will be used for picking an index in the productIds
+		// array. You need this step because the primary key ID in Products
+		// table might not be sequential.
+		randomPickedItem = productIds[randomItem()];
+
+		// Find the product name, description, picture URL and Amazon URL
+		// for the random-picked ID
+		var product = items[randomPickedItem].name,
+				description = items[randomPickedItem].description,
+				picurl = items[randomPickedItem].picurl,
+				amazonurl = items[randomPickedItem].amazonurl;
+
+		// Now, render the front page ("index.ejs") and pass the EJS variables
+		// that correspond with the above variables.
+		res.render("index.ejs", {
+			product: product,
+			description: description,
+			picurl: picurl,
+			amazonurl: amazonurl
+		});
+	}); 
+});
+
+// Route for a product administration page
 app.get("/admin", function(req, res) {
 	res.render("admin.ejs");
 })
